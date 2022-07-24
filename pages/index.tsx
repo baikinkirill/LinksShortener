78074c10ec6715be100e6b1cdbf051d87dded430
Kirill <kirill.baikin@gmail.com>
2022-07-24 15:48:27 +0300

    Added ESLint, removed SSR on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,34 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import MainPage from '../components/mainPage/MainPage';
-import getUserInfo from '../services/getUserInfo';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import MainPage from '../components/mainPage/MainPage'
+import React from 'react'
 
-export function DefaultHeader() {
- const HOST_URL = process.env.host_url || window.location.host;
+export function DefaultHeader () {
+  const HOST_URL = process.env.host_url || window.location.host
 
- return (
+  return (
   <Head>
    <title>{HOST_URL.toUpperCase()}</title>
-   <meta name="Лучший сокращатель ссылок для веб-сайта" />
-   <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-   <meta property="og:image" content="/cover.png" />
+   <meta name='Лучший сокращатель ссылок для веб-сайта' />
+   <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+   <meta property='og:image' content='/cover.png' />
   </Head>
- );
+  )
 }
 
 const Home: NextPage = (UserInfo: any) => {
- return (
+  return (
   <div>
    <DefaultHeader />
    <MainPage />
   </div>
- );
-};
+  )
+}
 
-export default Home;
+export default Home
 
-export function getServerSideProps(req: any) {
- return {
-  props: {
-   userInfo: getUserInfo(req.req),
-  },
- };
+export function getStaticProps (req: any) {
+  return {
+    props: {}
+  }
 }
--- pages/stats/[id].js
+++ pages/stats/[id].js
@@ -1,88 +1,90 @@
-import dynamic from 'next/dynamic';
-import styles from './stats.module.scss';
-import { DefaultHeader } from '../index';
-import { Fragment } from 'react';
+import dynamic from 'next/dynamic'
+import styles from './stats.module.scss'
+import { DefaultHeader } from '../index'
+import { Fragment } from 'react'
 
 const Line = dynamic(
- () => import('@ant-design/plots/lib/components/line/index'),
- {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
- }
-);
+  () => import('@ant-design/plots/lib/components/line/index'),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>
+  }
+)
 const Pie = dynamic(
- () => import('@ant-design/plots/lib/components/pie/index'),
- {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
- }
-);
+  () => import('@ant-design/plots/lib/components/pie/index'),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>
+  }
+)
 
-function LineParent(props) {
- let data = Object.keys(props.result.stat).map((e) => ({
-  Date: props.toTime
-   ? new Date(e * 1000).toLocaleTimeString('ru')
-   : new Date(e * 1000).toLocaleDateString('ru'),
-  Views: props.result.stat[e],
- }));
+function LineParent (props) {
+  const data = Object.keys(props.result.stat).map((e) => ({
+    Date: props.toTime
+      ? new Date(e * 1000).toLocaleTimeString('ru')
+      : new Date(e * 1000).toLocaleDateString('ru'),
+    Views: props.result.stat[e]
+  }))
 
- const config = {
-  data,
-  padding: 'auto',
-  xField: 'Date',
-  yField: 'Views',
-  xAxis: {
-   // type: 'timeCat',
-  },
-  smooth: true,
- };
+  const config = {
+    data,
+    padding: 'auto',
+    xField: 'Date',
+    yField: 'Views',
+    xAxis: {
+      // type: 'timeCat',
+    },
+    smooth: true
+  }
 
- return (
+  return (
   <div>
    <Line {...config} />
   </div>
- );
+  )
 }
 
-function PieParent(props) {
- const data = Object.keys(props.result.browsers).map((e) => ({
-  type: e,
-  value: props.result.browsers[e],
- }));
- const config = {
-  appendPadding: 10,
-  data,
-  angleField: 'value',
-  colorField: 'type',
-  radius: 0.8,
-  label: {
-   type: 'outer',
-   content: '{name} {percentage}',
-  },
-  interactions: [
-   {
-    type: 'pie-legend-active',
-   },
-   {
-    type: 'element-active',
-   },
-  ],
- };
- return <Pie {...config} />;
+function PieParent (props) {
+  const data = Object.keys(props.result.browsers).map((e) => ({
+    type: e,
+    value: props.result.browsers[e]
+  }))
+  const config = {
+    appendPadding: 10,
+    data,
+    angleField: 'value',
+    colorField: 'type',
+    radius: 0.8,
+    label: {
+      type: 'outer',
+      content: '{name} {percentage}'
+    },
+    interactions: [
+      {
+        type: 'pie-legend-active'
+      },
+      {
+        type: 'element-active'
+      }
+    ]
+  }
+  return <Pie {...config} />
 }
 
-export default function Stats(props) {
- console.log(props);
- return (
+export default function Stats (props) {
+  console.log(props)
+  return (
   <div className={styles.parent}>
    <DefaultHeader />
    <h1>{decodeURIComponent(props.result.link)}</h1>
    <div className={styles.content}>
-    {Object.keys(props.result.lastWeek.browsers).length === 0 ? (
+    {Object.keys(props.result.lastWeek.browsers).length === 0
+      ? (
      <h2 style={{ textAlign: 'center', marginTop: '20vh' }}>
       Статистика появится чуть позже
      </h2>
-    ) : (
+        )
+      : (
      <Fragment>
       <h2>За последние 7 дней</h2>
       <div className={styles.chartParent}>
@@ -103,34 +105,34 @@ export default function Stats(props) {
        </div>
       </div>
      </Fragment>
-    )}
+        )}
    </div>
   </div>
- );
+  )
 }
 
-export async function getServerSideProps(req) {
- const HOST_URL = process.env.host_url || req.req.headers.host;
- let response = await fetch(
-  'http://' + HOST_URL + '/api/stats/' + req.query.id
- );
- if (response.ok) {
-  let json = await response.json();
-  if (json.result) {
-   let res = json.result;
-   return {
-    props: {
-     result: res,
-    },
-   };
-  } else {
-   return {
-    notFound: true,
-   };
+export async function getServerSideProps (req) {
+  const HOST_URL = process.env.host_url || req.req.headers.host
+  const response = await fetch(
+    'http://' + HOST_URL + '/api/stats/' + req.query.id
+  )
+  if (response.ok) {
+    const json = await response.json()
+    if (json.result) {
+      const res = json.result
+      return {
+        props: {
+          result: res
+        }
+      }
+    } else {
+      return {
+        notFound: true
+      }
+    }
   }
- }
 
- return {
-  notFound: true,
- };
+  return {
+    notFound: true
+  }
 }
